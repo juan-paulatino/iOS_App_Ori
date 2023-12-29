e9efe13483b5371b30f6f7e78a83444e761078e6
Juan Carlos Paulino Fonseca <118320209+juan-paulatino@users.noreply.github.com>
2023-12-29 16:59:46 -0700

    Add files via upload

---
+++ pruebaApp.swift
@@ -0,0 +1,17 @@
+//
+//  pruebaApp.swift
+//  prueba
+//
+//  Created by Juan Carlos Paulino Fonseca Gomez on 12/29/23.
+//
+
+import SwiftUI
+
+@main
+struct pruebaApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
